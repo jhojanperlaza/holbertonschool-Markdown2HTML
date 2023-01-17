5632b231b6a552a657956d01b66eceb12c8abf6f
jhojan <jhojan_perlaza123>
2023-01-17 00:02:24 -0500

    [ADD] task 0

---
+++ markdown2html.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Script that generate a README file
+    -Start a script:
+        *First argument is the name of the Markdown file
+        *Second argument is the output file name
+"""
+import sys
+import os
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html")
+        exit(1)
+        
+    elif not os.path.isfile("./" + sys.argv[1]):
+        print("Missing <filename>")
+        exit(1)
