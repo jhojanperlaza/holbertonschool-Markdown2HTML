76728e95cabe4bf30d8a2db81efa944362937d9a
jhojan <jhojan_perlaza123>
2023-01-17 12:49:54 -0500

    [task 1]

---
+++ README.html
@@ -0,0 +1,5 @@
+<h1>Mytitle</h1>
+<h2>Mytitle2</h2>
+<h1>Mytitle3</h1>
+<h4>Mytitle4</h4>
+<h3>Mytitle5</h3>
--- markdown2html.py
+++ markdown2html.py
@@ -6,13 +6,42 @@
 """
 from sys import argv, stderr
 import os
+
+
 if __name__ == "__main__":
+
+    def heading(line):
+        heading_level = line.count('#')
+        line = line.replace('#', '').replace('\n', '').replace(' ', '')
+        str_return = "<h{}>{}</h{}>".format(heading_level, line, heading_level)
+        return str_return
+
+
+    expressions = {"#":heading}
+
     if len(argv) <= 2:
         stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
 
-    elif not os.path.exists(argv[1]):
-        stderr.write("Missing {}\n".format(argv[1]))
+    name_file = argv[1]
+
+    if not os.path.exists(name_file):
+        stderr.write("Missing {}\n".format(name_file))
         exit(1)
 
+    list_lines = []
+    with open(name_file) as f:
+        for line in f:
+            if line[0] in expressions:
+                new_line = expressions[line[0]](line)
+                list_lines.append(new_line)
+
+
+
+    with open(argv[2], 'w') as f:
+        for line in list_lines:
+            f.write(line)
+            f.write('\n')
+
     exit(0)
+
