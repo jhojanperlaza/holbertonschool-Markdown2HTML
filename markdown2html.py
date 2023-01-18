48a1336f559d0d1be6a7d2113b8dc54950819cdf
jhojan <jhojan_perlaza123>
2023-01-17 22:56:27 -0500

    [UPDATED] task 3

--- markdown2html.py
+++ markdown2html.py
@@ -18,16 +18,20 @@ if __name__ == "__main__":
         str_return = "<h{}>{}</h{}>".format(heading_level, line, heading_level)
         return str_return
 
-    def unordered_list(lines):
+    def unordered_list(lines, expression):
         list_returnt = []
         for line in lines:
             line = line.replace('-', '').replace('\n', '')
             list_returnt.append("\t<li>{}</li>\n".format(line))
 
-        str_return = "<ul>\n{}</ul>".format(''.join(list_returnt))
+        if expression == '-':
+            str_return = "<ul>\n{}</ul>".format(''.join(list_returnt))
+        else:
+            str_return = "<ol>\n{}</ol>".format(''.join(list_returnt))
+
         return str_return
 
-    expressions = {"-": unordered_list, "#": heading}
+    expressions = {"-": unordered_list, "#": heading, "*": unordered_list}
 
     if len(argv) <= 2:
         stderr.write("Usage: ./markdown2html.py README.md README.html\n")
@@ -48,15 +52,16 @@ if __name__ == "__main__":
 
         for line in file_lines:
             if line[0] in expressions:
-                if line[0] == '-':
+                if line[0] == '-' or line[0] == '*':
                     previous_expression = line[0]
                     listing_lines.append(line)
                     if line is last and len(listing_lines) != 0:
-                        new_line = expressions[line[0]](listing_lines)
+                        new_line = expressions[line[0]](listing_lines, line[0])
                         lines_to_write.append(new_line)
                     continue
                 if len(listing_lines) != 0:
-                    new_line = expressions[previous_expression](listing_lines)
+                    new_line = expressions[previous_expression](
+                        listing_lines, previous_expression)
                     lines_to_write.append(new_line)
                     listing_lines = []
 
